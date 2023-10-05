ccad05cd9ff9b95b9fd11b41c0ef089a508f3213
ranjan2794 <134986092+ranjan2794@users.noreply.github.com>
2023-10-05 14:18:13 +0530

    code added

---
+++ Task 1 -Portfolio/script.js
@@ -0,0 +1,9 @@
+// for closing menu
+function closeNav() {
+    document.getElementById("menu").style.width = "0%";
+}
+
+// for opening menu
+function openNav() {
+    document.getElementById("menu").style.width = "100%";
+}
